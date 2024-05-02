d1ad75f619f004a88991533777d3ff35272708bc
PRAKASH <prakashkandasamy4@gmail.com>
2024-05-03 00:53:17 +0530

    work-start

--- bus-route/src/Components/Contect.js
+++ bus-route/src/Components/Contect.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Home() {
+export default function Contect() {
   return (
     <div>
-      bye
+      hello
     </div>
   )
 }
---
+++ bus-route/src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../Components/Nav.css'
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='container'>
+    <h2 className='logo'>Logo</h2>
+      <nav className='nav-links'>
+        <NavLink className={'nav navitem1'} to='/'>Home</NavLink>
+        <NavLink className={'nav navitem2'} to='/about'>About</NavLink>
+        <NavLink className={'nav navitem3'} to='/contact'>Contact</NavLink>
+      </nav>
+      </div>
+    </div>
+  )
+}
