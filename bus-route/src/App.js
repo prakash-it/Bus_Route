20c19d2a6cfaefa64de62ea80d2221c16630f031
KGM <KGM@CASL6-56-184.kgisledu.com>
2024-05-03 19:24:18 +0530

    prjtas

--- bus-route/src/App.js
+++ bus-route/src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Contect from './Components/Contect';
 import Navbar from './Components/Navbar';
+import Admin from './Components/Admin';
 
 
 
@@ -15,8 +16,9 @@ function App() {
  <Routes>
   <Route path='/' element={<Home/>}/>
   <Route path='/about' element={<About/>}/>
-  <Route path='/contact' element={<Contect/>}/>  
+  <Route path='/contact' element={<Contect/>}/> 
  </Routes>
+ <Admin/>
     </div>
   );
 }
