49a5955e4e8b13b3de56d3d2f9b84e2efde59264
JAVAVEEN <naveenbsc.mca1518@gmail.com>
2024-05-03 13:00:36 +0530

    homepage

--- bus-route/src/App.js
+++ bus-route/src/App.js
@@ -6,15 +6,14 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Contect from './Components/Contect';
 
+
 function App() {
   return (
     <div className="App">
-     <Navbar/>
  <Routes>
   <Route path='/' element={<Home/>}/>
   <Route path='/about' element={<About/>}/>
-  <Route path='/contact' element={<Contect/>}/>
-  
+  <Route path='/contact' element={<Contect/>}/>  
  </Routes>
     </div>
   );
--- bus-route/src/Components/Home.js
+++ bus-route/src/Components/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import image1 from '../img1.jpg'
+import '../index.css'
 
 export default function Home() {
   return (
     <div>
-      bye
+      <div className="card cardDesign">
+        <img src={image1} className="card-img-top" alt="this is image 1"/>
+          <div className="card-body">
+            <h5 className="card-title">TownBus</h5>
+            <p className="card-text">"The greatest discoveries have come from people who have looked at a standard situation and seen it differently.” “I soon realized that no journey carries one far unless, as it extends into the world around us, it goes an equal distance into the world within.”</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
     </div>
   )
 }
+
