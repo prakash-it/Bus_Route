6381ffce1f8ff010d9a85fc39ca5393d108b24ac
PRAKASH <prakashkandasamy4@gmail.com>
2024-05-03 12:44:29 +0530

    nav

--- bus-route/src/Components/Navbar.js
+++ bus-route/src/Components/Navbar.js
@@ -5,9 +5,10 @@ import { NavLink } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className='navbar'>
+
         <div className='container'>
-    <h2 className='logo'>Logo</h2>
-      <nav className='nav-links'>
+        <h2 className='logo'>Logo</h2>
+        <nav className='nav-links'>
         <NavLink className={'nav navitem1'} to='/'>Home</NavLink>
         <NavLink className={'nav navitem2'} to='/about'>About</NavLink>
         <NavLink className={'nav navitem3'} to='/contact'>Contact</NavLink>
