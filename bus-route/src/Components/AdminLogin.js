479c6c96482e2944ac97b9ac56b22da102b44964
KGM <KGM@CASL6-57-185.kgisledu.com>
2024-05-06 16:27:28 +0530

    admin

---
+++ bus-route/src/Components/AdminLogin.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from './Auth'
+
+export default function AdminLogin() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [userlist, setUserlist] = useState([])
+    const [errmsg, setErrmsg] = useState('')
+    const navigate = useNavigate()
+    
+
+    useEffect(() => {
+        axios.get('http://localhost:1516/adminlogin')
+            .then(res => setUserlist(res.data))
+            .catch(err => console.log(err))
+    }, [])
+    
+    const handlelogin = (e) => {
+        e.preventDefault()
+        const user = userlist.find(x => x.email === email)
+        if (user) {
+            if (user.password === password) {
+            
+                navigate('/Admin')
+            } else {
+                setErrmsg("Incorrect Password")
+            }
+        } else {
+            setErrmsg("User not Found")
+        }
+    } 
+         
+    const userlogin = () => {
+        navigate('/Login')
+    }
+          
+   
+    return (
+        <div>
+            <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter the mail'/>
+            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter the password'/>
+            <button onClick={handlelogin}>Admin Login</button>
+            <button onClick={userlogin}>User Admin</button>
+
+            {errmsg}
+        </div>
+
+        
+
+    )
+}
