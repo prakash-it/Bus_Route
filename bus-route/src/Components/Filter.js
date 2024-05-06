902d3d4daf9e76d2496e8a07cb8c58985a006ede
PRAKASH <prakashkandasamy4@gmail.com>
2024-05-06 14:07:10 +0530

    Correct it

--- bus-route/src/Components/Filter.js
+++ bus-route/src/Components/Filter.js
@@ -8,12 +8,6 @@ import Table from 'react-bootstrap/Table';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 
-
-
-export default function Filter() {
-    const [from, setFrom] = useState('Starting point')
-    const [to, setTo] = useState('Ending Point')
-
 export default function Filters() {
     const [from, setFrom] = useState('Select your Starting point')
     const [to, setTo] = useState('Select your Ending Point')
@@ -36,7 +30,7 @@ export default function Filters() {
         setTable(true)
     }
 
-    const clearFunction=()=>{
+    const clearFunction = () => {
         setFrom('Starting point')
         setTo('End point')
         setTable(false)
@@ -75,98 +69,42 @@ export default function Filters() {
                         </Col>
                     </Row>
                 </form>
-
-        <div>
-             <h1 className='bus-head'>Select the Location to travel</h1>
-            <form className='filterForm'>
-                <Row>
-                    <Col>
-                        <Form.Select value={from} onChange={(e) => setFrom(e.target.value)}>
-                            <option>Select your Starting point</option>
-                            <option value='ukkadam'>Ukkadam</option>
-                            <option value='GH'>GH</option>
-                            <option value="RTO">RTO</option>
-                            <option value='gandhipuram'>Gandhipuram</option>
-                            <option value='singanallur'>Singanallur</option>
-                        </Form.Select>
-                    </Col>
-                    <Col>
-                        <Form.Select value={to} onChange={getDetails}>
-                            <option>Select your Ending Point</option>
-                            <option value='ukkadam'>Ukkadam</option>
-                            <option value='GH'>GH</option>
-                            <option value="RTO">RTO</option>
-                            <option value='gandhipuram'>Gandhipuram</option>
-                            <option value='singanallur'>Singanallur</option>
-                        </Form.Select>
-                    </Col>
-                </Row>
-            </form>
+            </div>
 
 
-                {/* <label>From:</label>
-        <select value={from} onChange={(e)=>setFrom(e.target.value)}>
-            <option value='ukkadam'>Ukkadam</option>
-            <option value="GH">GH</option>
-            <option value={'RTO'}>RTO</option>
-            <option value='gandhipuram'>Gandhipuram</option>
-        </select>
-        <label>To:</label>
-        <select value={to} onChange={getDetails}>
-            <option value='ukkadam'>Ukkadam</option>
-            <option value={'GH'}>GH</option>
-            <option value={"RTO"}>RTO</option>
-            <option value='gandhipuram'>Gandhipuram</option>
-        </select><br></br> */}
 
-                {table && filterData.length > 0(
-                    <Table responsive striped bordered hover size='lg' variant='dark'>
-                        <thead>
-                            <tr>
-                                <th>Bus.No</th>
-                                <th>Time</th>
-                                <th>Start</th>
-                                <th>End</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                              {filterData.sort((a, b) => a.time.localeCompare(b.time)).map((x) =>
-                               (<tr key={x.busno}>
-                                <td>{x.busno}</td>
-                                <td>{x.time}</td>
-                                <td>{x.from}</td>
-                                <td>{x.to}</td>
-                            </tr>))}
-                        </tbody>
-                    </Table>
-                )}
-                {table&&<Button className='col-sm-2' variant="light" onClick={clearFunction}>
-                    Clear
-                </Button>}
-            </div
 
-// {table   && filterData.length > 0 (
-//     <table>
-//         <thead>
-//             <tr>
-//                 <th>Bus.No</th>
-//                 <th>Time</th>
-//                 <th>Start</th>
-//                 <th>End</th>
-//             </tr>
-//         </thead>
-//         <tbody>
-//             {filterData.sort((a, b) => a.time.localeCompare(b.time)).map((x) => (
-//                 <tr key={x.busno}>
-//                     <td>{x.busno}</td>
-//                     <td>{x.time}</td>
-//                     <td>{x.from}</td>
-//                     <td>{x.to}</td>
-//                 </tr>
-//             ))}
-//         </tbody>
-//     </table>
-// )}
+            {table && filterData.length > 0 && (
+    <Table responsive striped bordered hover size='lg' variant='dark'>
+        <thead>
+            <tr>
+                <th>Bus.No</th>
+                <th>Time</th>
+                <th>Start</th>
+                <th>End</th>
+            </tr>
+        </thead>
+        <tbody>
+            {filterData.sort((a, b) => a.time.localeCompare(b.time)).map((x) => (
+                <tr key={x.busno}>
+                    <td>{x.busno}</td>
+                    <td>{x.time}</td>
+                    <td>{x.from}</td>
+                    <td>{x.to}</td>
+                </tr>
+            ))}
+        </tbody>
+    </Table>
+)}
+
+{table && (
+    <div className='col-sm-2'>
+        <Button variant="light" onClick={clearFunction}>
+            Clear
+        </Button>
+    </div>
+)}
+
 
         </div>
     )
